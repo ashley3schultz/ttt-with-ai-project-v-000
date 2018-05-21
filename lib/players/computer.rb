5c21cb9bcece2454a6671cd6e5e546f697832452
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 17:16:04 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,7 +16,7 @@ module Players
         o << board.cells[wc[0]] if board.cells[wc[0]] == !self.token && board.cells[wc[0]] != " "
         o << board.cells[wc[1]] if board.cells[wc[1]] == !self.token && board.cells[wc[1]] != " "
         o << board.cells[wc[2]] if board.cells[wc[2]] == !self.token && board.cells[wc[2]] != " "
-        wc.detect {|c| board.cells[c] == " "} if o.size == 2 }}
+        wc.detect {|c| board.cells[c] == " "} if o.size == 2 }
     end
     
     def defence
@@ -25,7 +25,7 @@ module Players
         d << board.cells[wc[0]] if board.cells[wc[0]] == self.token
         d << board.cells[wc[1]] if board.cells[wc[1]] == self.token
         d << board.cells[wc[2]] if board.cells[wc[2]] == self.token
-        wc.detect {|c| board.cells[c] == " "} if d.size == 2 }}
+        wc.detect {|c| board.cells[c] == " "} if d.size == 2 }
     end 
     
     def random
