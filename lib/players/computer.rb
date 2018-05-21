38f56e3a2118af3163dc52616cc8d28d0574c6ad
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 18:11:19 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,15 +7,14 @@ module Players
       nb = Board.new
       nb.cells = ["X", " ", "O", " ", "X", "O", " ", " ", " "]
 
-      if defence(nb)
-        resault = defence(nb).detect {|c| nb.cells[c] == " "}
-      elsif offence(nb)
-        resault = offence(nb).detect {|c| nb.cells[c] == " "}
+      if defence(board)
+        resault = defence(board).detect {|c| board.cells[c] == " "}
+      elsif offence(board)
+        resault = offence(board).detect {|c| board.cells[c] == " "}
       else 
-        resault = random(nb)
+        resault = random(board)
       end
       resault
-      binding.pry
     end
     
     def defence(board)
@@ -30,9 +29,9 @@ module Players
     def offence(board)
       Game::WIN_COMBINATIONS.detect {|wc| 
         o = []
-        o << board.cells[wc[0]] if board.cells[wc[0]] == !self.token && board.cells[wc[0]] != " "
-        o << board.cells[wc[1]] if board.cells[wc[1]] == !self.token && board.cells[wc[1]] != " "
-        o << board.cells[wc[2]] if board.cells[wc[2]] == !self.token && board.cells[wc[2]] != " "
+        o << board.cells[wc[0]] if board.cells[wc[0]] != self.token && board.cells[wc[0]] != " "
+        o << board.cells[wc[1]] if board.cells[wc[1]] != self.token && board.cells[wc[1]] != " "
+        o << board.cells[wc[2]] if board.cells[wc[2]] != self.token && board.cells[wc[2]] != " "
         wc.detect {|c| board.cells[c] == " "} if o.size == 2 }
     end
     
