a68827ee73cb5855e14b1a89a102f7528b6cab5c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 16:43:06 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,7 +26,7 @@ module Players
         o << board.cells[wc[0]] if board.cells[wc[0]] == !self.token && board.cells[wc[0]] != " "
         o << board.cells[wc[1]] if board.cells[wc[1]] == !self.token && board.cells[wc[1]] != " "
         o << board.cells[wc[2]] if board.cells[wc[2]] == !self.token && board.cells[wc[2]] != " "
-        o.size == 2}}
+        wc if o.size == 2}}
     end
       
     resault = wc.detect {|i| !o.include?(i)}
@@ -37,8 +37,7 @@ module Players
         d << board.cells[wc[0]] if board.cells[wc[0]] == self.token
         d << board.cells[wc[1]] if board.cells[wc[1]] == self.token
         d << board.cells[wc[2]] if board.cells[wc[2]] == self.token
-        d.size == 2}}
-      
+        wc if d.size == 2}}
     end 
     
     
