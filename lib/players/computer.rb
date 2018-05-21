a9c89144a4a5efd0ce60b79a7a8a76cbf97a1dd3
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 16:46:22 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,16 +3,8 @@ module Players
   class Computer < Player
   
     def move(board)
-      WIN_COMINATIONS.detect {|wc| wc.
-      
-      
-      WIN_COMBINATIONS.detect {|wc| 
-    o = []
-    
-    o << board.cells[wc[0]] if board.cells[wc[0]] == !self.token && != " "
-    o << board.cells[wc[1]] if board.cells[wc[1]] == !self.token && != " "
-    o << board.cells[wc[2]] if board.cells[wc[2]] == !self.token && != " "
-    
+      if offense != nil 
+        #plays the extra number
     if o.size == 2
       resault = wc.detect {|i| !o.include?(i)}
     elsif d.size == 2
