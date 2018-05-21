6d719f650e2827dee92562e46d3d96b98f3b2a99
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 16:14:35 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,7 +3,8 @@ module Players
   class Human < Player
     
     def move(board)
-     puts current_player "Please enter 1-9"
+     puts "Please enter 1-9"
+     
      num = gets
      num
     end 
