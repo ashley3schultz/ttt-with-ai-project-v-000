f8e2f58cf69f47e47afadba7b1116f7632a691be
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 16:22:06 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,16 @@ module Players
   
     def move(board)
       WIN_COMINATIONS.detect {|wc| wc.
+      
+      
+      WIN_COMBINATIONS.detect {|wc| 
+    ck = []
+    ck << board.cells[wc[0]] if board.cells[wc[0]] == !self.token 
+    ck << board.cells[wc[1]]
+    ck << board.cells[wc[2]]
+    wc if ck.all? {|x|  x == "X"} || ck.all? {|o| o == "O"}}
+    
+    
       rand(1..9).to_s
     end
     
