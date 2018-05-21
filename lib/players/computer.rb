195e4fa27a8bbb50a12ed9cd216ef171d39c6d9f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-21 16:59:25 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,17 +3,14 @@ module Players
   class Computer < Player
   
     def move(board)
-      if offense != nil 
-        #plays the extra number
-      elsif offence != nil
-        #plays the extra number
-      else
+      if defense != nil 
+        defense.detect {|c| board.cells[c] == " "}
         
-    if o.size == 2
-      resault = wc.detect {|i| !o.include?(i)}
-    elsif d.size == 2
-   
-      rand(1..9).to_s
+      elsif offence != nil
+        offense.detect {|c| board.cells[c] == " "}
+      else
+        rand(1..9).to_s
+      end
     end
     
     def offence
@@ -22,7 +19,7 @@ module Players
         o << board.cells[wc[0]] if board.cells[wc[0]] == !self.token && board.cells[wc[0]] != " "
         o << board.cells[wc[1]] if board.cells[wc[1]] == !self.token && board.cells[wc[1]] != " "
         o << board.cells[wc[2]] if board.cells[wc[2]] == !self.token && board.cells[wc[2]] != " "
-        wc if o.size == 2}}
+        wc.detect {|c| board.cells[c] == " "} if o.size == 2 }}
     end
       
     resault = wc.detect {|i| !o.include?(i)}
@@ -33,7 +30,7 @@ module Players
         d << board.cells[wc[0]] if board.cells[wc[0]] == self.token
         d << board.cells[wc[1]] if board.cells[wc[1]] == self.token
         d << board.cells[wc[2]] if board.cells[wc[2]] == self.token
-        wc if d.size == 2}}
+        wc.detect {|c| board.cells[c] == " "} if d.size == 2 }}
     end 
     
     
